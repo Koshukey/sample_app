e866d7f942e01b7b74ca893bbf5a854b50300265
Koshukey <koshuke0217@icloud.com>
2018-04-10 21:40:13 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home |#{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help |#{@base_title}"
   end
 
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+#static_pages_about_urlなどはroutes.rbに記載されている内容に基づいて使用可能になる
+#これらはrails ヘルパーと呼ばれる
 end
