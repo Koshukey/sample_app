48bff855e788bc192a5674fb3ed24d0e86e9a996
Koshukey <koshuke0217@icloud.com>
2018-04-11 20:17:02 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+    #params[:id]は文字列型であるがfindメソッドが整数に変更する
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    # @user = User.new(params[:user]) と
+    # @user = User.new(name: "Foo Bar", email: "foo@invalid",
+    #              password: "foo", password_confirmation: "bar")
+    # と全く同じ
+    # なぜこうなるかといえばform_forで生成されるname属性がuser[email]
+    # のようになっているから
+    if @user.save
+      flash[:success] = "Welcome to the Simple but difficult app!"
+      #falshはrailsで用意されている変数
+      redirect_to @user
+      #redirect_to user_url(@user)と同じ意味
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    # Digestライブラリのhexdigestメソッドを使うことによってMD5のハッシュ化が実現
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
